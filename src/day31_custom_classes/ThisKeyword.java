2680ff224ddb2e1fc946836126e042e5e5377181
feyruzshahmuradov <feyruz@loopcamp.io>
2023-04-19 22:23:13 -0400

    day31 work

---
+++ src/day31_custom_classes/ThisKeyword.java
@@ -0,0 +1,18 @@
+package day31_custom_classes;
+
+public class ThisKeyword {
+
+    // this vs this()
+
+    int a = 100;
+
+
+    public ThisKeyword (int a){
+        a = 400;
+        this.a = a;
+    }
+
+}
+
+
+
